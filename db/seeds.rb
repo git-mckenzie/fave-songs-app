74e24a3245c296f8c84b5225f7d11bc8b8ae62de
Jason McKenzie <jmac.atx@gmail.com>
2021-06-21 12:47:11 -0500

    adds CRU REST actions

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Song.create(title: "song1", album: "album1", artist: "artist1", year: 1984)
+Song.create(title: "song2", album: "album2", artist: "artist2", year: 2021)
